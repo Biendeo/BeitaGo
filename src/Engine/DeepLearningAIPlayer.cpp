5656ad7188a88b8e2a3bdeda0adc9cdb9e0193c5
Thomas Moffet <pantherman1996@gmail.com>
2018-09-30 20:11:30 +1000

    Made the input vector a little quicker.

--- src/Engine/DeepLearningAIPlayer.cpp
+++ src/Engine/DeepLearningAIPlayer.cpp
@@ -40,22 +40,13 @@ namespace BeitaGo {
 		std::array<bool, INPUT_VECTOR_SIZE> inputVector;
 		int index = 0;
 		for (int i = 0; i < 2 * HISTORY_USED; ++i) {
+			std::vector<std::vector<Color>> historyBoard = board.GetPreviousLayout(i / 2);
 			for (int y = 0; y < board.GetDimensions().Y(); ++y) {
 				for (int x = 0; x < board.GetDimensions().X(); ++x) {
 					if (i % 2 == 0) {
-						if (i == 0) {
-							inputVector[index] = board.GetTile(Grid2(x, y)) == Color::Black;
-						} else {
-							std::vector<std::vector<Color>> historyBoard = board.GetPreviousLayout(i / 2);
 							inputVector[index] = historyBoard[x][y] == Color::Black;
-						}
 					} else {
-						if (i == 0) {
-							inputVector[index] = board.GetTile(Grid2(x, y)) == Color::White;
-						} else {
-							std::vector<std::vector<Color>> historyBoard = board.GetPreviousLayout(i / 2);
 							inputVector[index] = historyBoard[x][y] == Color::White;
-						}
 					}
 					++index;
 				}
